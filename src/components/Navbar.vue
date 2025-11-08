9e8b932853b7966d238b0f0fe67ebf75eeecf76b
Sergio <sergio.boffi@epfl.ch>
2025-11-08 16:33:52 +0100

    cambiato nome al logo

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <Menubar :model="items" class="shadow-2 mb-4">
     <template #start>
-      <img alt="logo" src="../assets/logo.png" height="40" class="mr-2" />
+      <img alt="logo" src="../assets/Logo.png" height="40" class="mr-2" />
       <span class="text-xl font-semibold">Nano Garden</span>
     </template>
   </Menubar>
